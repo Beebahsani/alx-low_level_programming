7391ecd12aadff9f74cdc4513dacf84f51d017c5
Umi Habibat Sani <beebatsani@gmail.com>
2022-09-12 16:57:41 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,34 +1,16 @@
-#include <stdio.h>                                                                                                                             
-
-                                                                                                                                               
-
-/**                                                                                                                                            
-
-* main - print the string in the put function                                                                                                  
-
-*                                                                                                                                              
-
-* Description: using the main function                                                                                                         
-
-* this programme prints "programming is like building multilingual puzzle                                                                      
-
-* Return: 0                                                                                                                                    
-
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
 */
+
 int main(void)
 {
-char c;
-int i;
-long li;
-long long lli;
-float f;
-
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of a int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(li));
-
-printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-
-printf("Size of a float: %d byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeoof (long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
