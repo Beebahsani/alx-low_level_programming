5d86689e375d570964f2d9f291f0b75fce83d352
Umi Habibat Sani <beebatsani@gmail.com>
2022-09-12 17:06:25 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,34 @@
-#include <stdio.h>
-/**
-* main - Entry point
-*
-* Return: Always 0 (success)
-*/
+#include <stdio.h>                                                                                                                             
 
-int main(void)
-{
+/**                                                                                                                                            
+
+* main - Entry point                                                                                                                           
+
+*                                                                                                                                              
+
+* Return: Always 0 (success)                                                                                                                   
+
+*/                                                                                                                                             
+
+                                                                                                                                               
+
+int main(void)                                                                                                                                 
+
+{ 
+char c;
+int i;
+long li;
+long long lli;
+float f;
 printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeoof (long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
-}
+printf("Size of an int: %d byte(s)\n", sizeof(int));                                                                                           
+
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));                                                                                  
+
+printf("Size of a long long int: %d byte(s)\n", sizeoof (long long int));                                                                      
+
+printf("Size of a float: %d byte(s)\n", sizeof(float));                                                                                        
+
+return (0);                                                                                                                                    
+
+}            
