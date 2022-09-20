3e3c48f7a8851d0ff397f14f63bd419cded809d6
Umi Habibat Sani <beebatsani@gmail.com>
2022-09-20 13:04:28 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
 *_strlen - returns the length of a string
@@ -11,6 +11,5 @@ int _strlen(char *s)
 int len = 0;
 while (*(s + len) != '\0')
 len++;
-
 return (len);
 }
